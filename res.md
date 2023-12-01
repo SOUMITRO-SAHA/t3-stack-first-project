434391777ba7d37f6b51afaa57e0f42b11824cbd
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-12-01 13:07:34 +0530

    Initial Commit

---
+++ res.md
@@ -0,0 +1,4 @@
+# Resources
+
+Figma File Link: https://www.figma.com/file/Bkz0ewKqWyIOCt2sewUnhW/Vidchill---Design?type=design&mode=design&t=rg1o9Y2OlcCQ2iau-0
+
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,12 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -48,20 +49,28 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: env.EMAIL_FROM
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
+    })
   ],
+  theme: {
+    colorScheme: "auto",
+    brandColor: "",
+    logo: "",
+    buttonText: "",
+  }
 };
 
 /**
